5c94cf0966b41d162216d4b99bb908bfc2a2f317
CrossOriGenes <dsnehodipto@gmail.com>
2024-09-04 20:26:44 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -36,10 +36,10 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
